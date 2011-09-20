52014f0ffa1256f8556f3766f68d44ff4e82ee32
Laurens Van Houtven <_@lvh.cc>
2011-09-21 00:00:08 +0800

    Lots of preliminary work on the mock sandbox

--- txievery/test/mocksandbox.py
+++ txievery/test/mocksandbox.py
@@ -4,18 +4,27 @@ A mock Paypal sandbox.
 import urllib
 import urlparse
 
-from twisted.web import server
+from twisted.web import resource
 
 
 class APICallDetails(object):
     def __init__(self, pairs):
         self.pairs = pairs
-        self.__iter__ = pairs.__iter__
-        self.__getitem__ = dict(pairs).__getitem__
+        self._pairsMap = pairsMap = dict(pairs)
+        if len(pairsMap) != len(pairs):
+            duplicates = [p for p in pairs if p[0] in pairsMap]
+            raise ValueError("Duplicate pairs: {}".format(duplicates))
+
+    def __iter__(self):
+        return iter(self.pairs)
+
+
+    def __getitem__(self, key):
+        return self._pairsMap[key]
 
 
 
-class Endpoint(server.Resource):
+class Endpoint(resource.Resource):
     def __init__(self):
         self.sandbox = Sandbox()
 
@@ -38,8 +47,10 @@ class Sandbox(object):
     def __init__(self):
         self._checkouts = {}
 
+
     def do_SetExpressCheckout(self, details):
         pass
+
 
     def do_GetExpressCheckoutDetails(self, details):
         pass
---
+++ txievery/test/test_mocksandbox.py
@@ -0,0 +1,64 @@
+"""
+Tests for the mock sandbox.
+"""
+import mock
+import StringIO
+import urllib
+import urlparse
+
+from twisted.trial import unittest
+
+from txievery.test import mocksandbox
+
+
+class APICallDetailsTest(unittest.TestCase):
+    def test_itemAccess(self):
+        pairs = [("a", "b"), ("c", "d")]
+        acd = mocksandbox.APICallDetails(pairs)
+        self.assertEquals(acd["a"], "b")
+        self.assertEquals(acd["c"], "d")
+        self.assertRaises(KeyError, lambda: acd["z"])
+
+
+    def test_iteration(self):
+        pairs = [("a", "b"), ("c", "d")]
+        self.assertEquals(list(mocksandbox.APICallDetails(pairs)), pairs)
+
+
+    def test_duplicatePairs(self):
+        pairs = [("a", "b"), ("a", "c")]
+        self.assertRaises(ValueError, mocksandbox.APICallDetails, pairs)
+
+
+
+class EndpointTest(unittest.TestCase):
+    def setUp(self):
+        self.endpoint = mocksandbox.Endpoint()
+
+    def _sendRequest(self, pairs):
+        request = type("FakeRequest", (), {})()
+        content = urllib.urlencode(pairs)
+        request.content = StringIO.StringIO(content)
+        response = self.endpoint.render_POST(request)
+        return urlparse.parse_qsl(response)
+
+    
+    def test_dispatch(self):
+        sandbox = self.endpoint.sandbox = mock.Mock()
+        expectedResponse = [("GARAAARASDFSSDF!!!!", "ARAWERSS")]
+        sandbox.do_AWESOME.return_value = expectedResponse
+        pairs = [("METHOD", "AWESOME"), ("DOCUMENTATION", "ARCANE")]
+        response = self._sendRequest(pairs)
+
+        details = sandbox.do_AWESOME.call_args[0][0]
+        self.assertEqual(details.pairs, pairs)
+        self.assertEqual(response, expectedResponse)
+
+
+    def test_unknownMethod(self):
+        pairs = [("METHOD", "BOGUS")]
+        self.assertRaises(ValueError, self._sendRequest, pairs)
+
+
+    def test_missingMethod(self):
+        self.assertRaises(ValueError, self._sendRequest, [])
